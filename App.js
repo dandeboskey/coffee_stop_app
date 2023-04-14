905ae2beee16e80605c04189831b1e1ac3dfcc3b
Ria Vora <riavora11@gmail.com>
2023-04-14 14:35:01 -0700

    worked on App.js

--- App.js
+++ App.js
@@ -1,15 +1,5 @@
 /*
 Final Project
-Template code for CS160
-Prepared by Shm Garanganao Almeda 
-Code referenced from: 
-CS160 Summer 2021
-https://www.digitalocean.com/community/tutorials/how-to-create-a-web-server-in-node-js-with-the-http-module"
-https://expressjs.com/en/starter/hello-world.html
-https://codeforgeek.com/render-html-file-expressjs/
-https://stackoverflow.com/questions/32257736/app-use-express-serve-multiple-html
-Photo Credits:
-Bunny by Satyabratasm on Unsplash <https://unsplash.com/photos/u_kMWN-BWyU>
 */
 
 //Node modules to *require*
@@ -56,61 +46,6 @@ app.get('/t6', function (req, res) {
 app.get('/t7', function (req, res) {
     res.sendFile(publicPath + '/t7.html');
 });
-
-
-// -- Task 5 Navigation -- //
-app.get('/t5', function (req, res) {
-    res.sendFile(publicPath + '/t5/index.html');
-});
-app.get('/t5/a', function (req, res) {
-    res.sendFile(publicPath + '/t5/a.html');
-});
-app.get('/t5/b', function (req, res) {
-    res.sendFile(publicPath + '/t5/b.html');
-});
-app.get('/t5/c', function (req, res) {
-    res.sendFile(publicPath + '/t5/c.html');
-});
-app.get('/t5/d', function (req, res) {
-    res.sendFile(publicPath + '/t5/d.html');
-});
-app.get('/t5/e', function (req, res) {
-    res.sendFile(publicPath + '/t5/e.html');
-});
-app.get('/t5/f', function (req, res) {
-    res.sendFile(publicPath + '/t5/f.html');
-});
-
-// -- Task 8 Navigation -- //
-app.get('/t8', function (req, res) {
-    res.sendFile(publicPath + '/t8/index.html');
-});
-app.get('/t8/a', function (req, res) {
-    res.sendFile(publicPath + '/t8/a.html');
-});
-app.get('/t8/b', function (req, res) {
-    res.sendFile(publicPath + '/t8/b.html');
-});
-app.get('/t8/c', function (req, res) {
-    res.sendFile(publicPath + '/t8/c.html');
-});
-app.get('/t8/d', function (req, res) {
-    res.sendFile(publicPath + '/t8/d.html');
-});
-app.get('/t8/e', function (req, res) {
-    res.sendFile(publicPath + '/t8/e.html');
-});
-app.get('/t8/f', function (req, res) {
-    res.sendFile(publicPath + '/t8/f.html');
-});
-app.get('/t8/g', function (req, res) {
-    res.sendFile(publicPath + '/t8/g.html');
-});
-app.get('/t8/h', function (req, res) {
-    res.sendFile(publicPath + '/t8/h.html');
-});
-
-
 
 
 //run this server by entering "node App.js" using your command line. 
